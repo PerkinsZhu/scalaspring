f5d5740df0df332b48ee55976827686770668143
PerkinsZhu <perkinszhu@sina.cn>
2018-07-01 22:33:25 +0800

    init

---
+++ src/main/scala/HelloScalaSpring.scala
@@ -0,0 +1,25 @@
+import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
+
+/**
+  *
+  * Created by PerkinsZhu on 7/1/18 9:14 PM
+  *
+  **/
+class HelloScalaSpring extends HttpServlet {
+
+  override def doGet(req: HttpServletRequest
+                     , res: HttpServletResponse
+                    ) {
+
+    res.setContentType("text/html")
+    res.setCharacterEncoding("UTF-8")
+
+    val responseBody: String =
+      """<html>
+        |  <body>
+        |    <h1>Hello, world!</h1>
+        |  </body>
+        |</html>""".stripMargin
+    res.getWriter.write(responseBody)
+  }
+}
