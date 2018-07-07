ecd4b2e29f98ada185dcddfb9a3473c7262c9ce1
Perkins Zhu <perkinszhu@sina.cn>
2018-07-07 17:42:16 +0800

    实现自定义注解的扫描

--- src/main/scala/com/perkinszhu/classes/ClassHelper.scala
+++ src/main/scala/com/perkinszhu/classes/ClassHelper.scala
@@ -3,6 +3,7 @@ package com.perkinszhu.classes
 import java.io.File
 import java.net.{JarURLConnection, URLDecoder}
 
+import com.perkinszhu.annotation.item.{Action, Inject, Service}
 import org.slf4j.LoggerFactory
 
 import scala.collection.mutable
@@ -12,6 +13,32 @@ import scala.reflect.runtime.universe._
   * Created by PerkinsZhu on 2018/7/7 11:26
   **/
 
+
+class ClassHelper(packagePath: String) {
+  val logger = LoggerFactory.getLogger(this.getClass)
+  val typeTagSet = TypeTagUtil.getClassSet(packagePath)
+
+  val serviceTypeTagSet = mutable.Set.empty[TypeTag[_]]
+  val actionTypeTagSet = mutable.Set.empty[TypeTag[_]]
+  val injectTypeTagSet = mutable.Set.empty[TypeTag[_]]
+
+
+    val serviceType = typeOf[Service]
+    val actionType = typeOf[Action]
+    val injectType = typeOf[Inject]
+    typeTagSet.foreach(tag => {
+      tag.tpe.typeSymbol.asClass.annotations.foreach(anno => {
+        anno.tree.tpe match {
+          case serviceType => serviceTypeTagSet += tag
+          case actionType => actionTypeTagSet += tag
+          case injectType => injectTypeTagSet += tag
+          case other => logger.error(s"未知注解[$other]类型")
+        }
+      })
+    })
+
+
+}
 
 object TypeTagUtil {
 
@@ -36,7 +63,7 @@ object TypeTagUtil {
           while (entries.hasMoreElements) {
             val entry = entries.nextElement()
             if (entry.getName.endsWith(".class")) {
-              //              javClassSet.add(typeTagBuilder (entry.getName))
+              //javClassSet.add(typeTagBuilder (entry.getName))
             }
           }
           classSet ++= javClassSet
--- src/test/scala/comscala/classes/TypeTagUtilTest.scala
+++ src/test/scala/comscala/classes/TypeTagUtilTest.scala
@@ -1,7 +1,11 @@
 package comscala.classes
 
-import com.perkinszhu.classes.TypeTagUtil
+import javassist.bytecode.stackmap.TypeTag
+
+import com.perkinszhu.classes.{ClassHelper, TypeTagUtil}
 import org.junit.Test
+
+import reflect.runtime.universe._
 
 /**
   * Created by PerkinsZhu on 2018/7/7 13:42
@@ -12,6 +16,18 @@ class TypeTagUtilTest {
   def testGetClassSet(): Unit = {
     val result = TypeTagUtil.getClassSet("com.user")
     println(result)
+    val annotations = result.map(tag => {
+      println(tag.tpe.typeSymbol.asClass.annotations)
+      println(tag.tpe.decls)
+    })
+  }
+
+  @Test
+  def testClassHelper(): Unit = {
+    val classHelper = new ClassHelper("com.user")
+    println(classHelper.serviceTypeTagSet)
+    println(classHelper.actionTypeTagSet)
+    println(classHelper.injectTypeTagSet)
   }
 
 }
